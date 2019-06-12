63942bd76fd02493dbc15330310ba43d04d5f8d0
Shuheng Johnny Liu <wish1104@outlook.com>
2019-06-13 05:31:33 +0800

    solved p88 merge sorted array

---
+++ p0088-merge-sorted-array.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+const merge = function(nums1, m, nums2, n) {
+  let k = m + n - 1;
+  let i = m - 1;
+  let j = n - 1;
+
+  while (k >= 0) {
+    console.log(`${nums1[i]} v.s. ${nums2[j]}`);
+    if (nums1[i] > nums2[j]) {
+      nums1[k --] = nums1[i --];
+    } else {
+      nums1[k --] = nums2[j --];
+    }
+    if (i < 0 || j < 0) break;
+  }
+  while (j >= 0) {
+    nums1[k --] = nums2[j --];
+  }
+};
+
+const nums1 = [1, 3, 5, 0, 0, 0, 0, 0];
+const m = 3;
+const nums2 = [-1, 0, 2, 4, 6];
+const n = 5;
+merge(nums1, m, nums2, n);
+console.log(nums1);
