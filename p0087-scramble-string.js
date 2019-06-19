ec0a9e90a8fa0bd51d4ec6770ebd6f5d0022689a
Shuheng Johnny Liu <wish1104@outlook.com>
2019-06-20 06:04:16 +0800

    solved p87 scramble string

--- p0087-scramble-string.js
+++ p0087-scramble-string.js
@@ -7,43 +7,51 @@ const isScramble = function(s1, s2) {
   // console.log(`checking '${s1}' and '${s2}'`);
   if (s1 === s2) return true;
   if (s1.length !== s2.length) return false;
-  if (s1.length === 1) return (s1 === s2);
+  if (s1.length === 1) return s1 === s2;
+  if (s1.length === 2) return s1 === s2 || (s1[0] === s2[1] && s1[1] === s2[0]);
 
   const deltas = new Array(26).fill(0);
-  for (let i = 0; i < s1.length; i ++) {
-    const id1 = getLetterIndex(s1[i]);
-    const id2 = getLetterIndex(s2[i]);
-    deltas[id1] ++;
-    deltas[id2] --;
+  for (let i = 0; i < s1.length; i++) {
+    deltas[s1.charCodeAt(i) - 97]++;
+    deltas[s2.charCodeAt(i) - 97]--;
   }
 
   for (const delta of deltas) {
     if (delta !== 0) {
+      console.log(`'${s1}' and '${s2}' differ`);
       return false;
     }
   }
 
-  for (let split = 1; split < s1.length; split ++) {
-    const left1 = s1.substring(0, split);
-    const right1 = s1.substring(split);
-    const left2 = s2.substring(0, split);
-    const right2 = s2.substring(split);
-    if (isScramble(left1, left2) && isScramble(right1, right2)) return true;
+  for (let i = 1; i < s1.length; i++) {
+    if (
+      isScramble(s1.substring(0, i), s2.substring(0, i)) &&
+      isScramble(s1.substring(i), s2.substring(i))
+    ) {
+      console.log(`'${s1}' and '${s2}' match`);
+      return true;
+    }
 
-    const left3 = s2.substring(0, s2.length - split);
-    const right3 = s2.substring(s2.length - split);
-    if (isScramble(left1, right3) && isScramble(right1, left3)) return true;
+    if (
+      isScramble(s1.substring(0, i), s2.substring(s2.length - i)) &&
+      isScramble(s1.substring(i), s2.substring(0, s2.length - i))
+    ) {
+      console.log(`'${s1}' and '${s2}' match`);
+      return true;
+    }
   }
 
+  console.log(`'${s1}' and '${s2}' differ`);
   return false;
 };
 
-const getLetterIndex = (char) => char - 'a';
 
-console.log(isScramble('great', 'rgeat'));
-console.log(isScramble('', ''));
-console.log(isScramble('a', 'a'));
-console.log(isScramble('a', 'b'));
-console.log(isScramble('ab', 'ba'));
-console.log(isScramble('abc', 'dba'));
-console.log(isScramble('abb', 'bba'));
+// console.log(isScramble('great', 'rgeat'));
+// console.log(isScramble('', ''));
+// console.log(isScramble('a', 'a'));
+// console.log(isScramble('a', 'b'));
+// console.log(isScramble('ab', 'ba'));
+// console.log(isScramble('abc', 'dba'));
+// console.log(isScramble('abb', 'bba'));
+
+console.log(isScramble('abcdefghijklmn', 'efghijklmncadb'));
